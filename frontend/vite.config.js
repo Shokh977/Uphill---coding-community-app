31d3eed5d5829e514dbccf09d243fedf26409b20
Shokhrukh <uphill7165@gmail.com>
2024-10-10 22:30:45 +0900

    updated the vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // Adjust 'src' based on your project structure
+export default {
+  build: {
+    outDir: 'dist', 
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString(); 
+          }
+        },
+      },
     },
   },
-});
+};
