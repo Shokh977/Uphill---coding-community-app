8a437cf575f01dd945fbd76a14308ab2bbf626a8
Shokhrukh <uphill7165@gmail.com>
2024-10-10 23:07:36 +0900

    updated actual links

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
+const corsOptions = {
+  origin: process.env.NODE_ENV === "production" ? "https://uphill-coding-community-app-zng7.onrender.com/" : "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json()); // allows to parse incoming requests : req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
@@ -24,7 +27,7 @@ app.use("/api/todos", todoRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.length("*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = process.env.NODE_ENV === 'production' ? "https://uphill-coding-community.onrender.com/" : "http://localhost:5000";
 
 axios.defaults.withCredentials = true;
 
