74acc2f734886f44690378ed4798da7df399e484
Shokhrukh <113904188+Shokh977@users.noreply.github.com>
2024-10-14 00:41:00 +0900

    Update toDoStore.js

--- frontend/src/store/toDoStore.js
+++ frontend/src/store/toDoStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "https://uphill-coding-community.onrender.com/api/todos";
+const API_URL = "https://api.uphill-tech.com";
 
 
 export const useTodoAuth = create((set) => ({
