033e4c33edfa6ca58cc0bdf4ea69dbbcefe08b6d
Shokhrukh <uphill7165@gmail.com>
2025-03-27 07:11:24 +0900

    resolved merge conflict

--- backend/index.js
+++ backend/index.js
@@ -24,13 +24,10 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
-<<<<<<< HEAD
 app.use('/', (req, res) => {
   res.send('API is working');
 });
-=======
 app.use('/', (req, res) => res.send('API is working'));
->>>>>>> 89d56c4109b112a3345217ea60e3fe76a2240c8c
 
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
